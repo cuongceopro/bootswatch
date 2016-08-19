18dd0f5cedfa032904538f194c79eee49ea85865
cuongceopro <cuongceopro@gmail.com>
2016-08-19 19:58:19 +0900

    update

---
+++ Vietnamguide/test/map.js
@@ -0,0 +1,39 @@
+/********************************************************************************
+
+	SYNCER 〜 知識、感動をみんなと同期(Sync)するブログ
+
+	* 配布場所
+	https://syncer.jp/google-maps-javascript-api-matome
+
+	* 最終更新日時
+	2015/08/03 19:24
+
+	* 作者
+	あらゆ
+
+	** 連絡先
+	Twitter: https://twitter.com/arayutw
+	Facebook: https://www.facebook.com/arayutw
+	Google+: https://plus.google.com/114918692417332410369/
+	E-mail: info@syncer.jp
+
+	※ バグ、不具合の報告、提案、ご要望など、お待ちしております。
+	※ 申し訳ありませんが、ご利用者様、個々の環境における問題はサポートしていません。
+
+********************************************************************************/
+
+
+// キャンパスの要素を取得する
+var canvas = document.getElementById( 'map-canvas' ) ;
+
+// 中心の位置座標を指定する
+var latlng = new google.maps.LatLng( 35.792621 , 139.806513 );
+
+// 地図のオプションを設定する
+var mapOptions = {
+	zoom: 15 ,				// ズーム値
+	center: latlng ,		// 中心座標 [latlng]
+};
+
+// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+var map = new google.maps.Map( canvas , mapOptions ) ;
