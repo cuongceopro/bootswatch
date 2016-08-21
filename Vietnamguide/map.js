2b82e650ded0bcb147280a58c713aa26db17b9d7
cuongceopro <cuongceopro@gmail.com>
2016-08-21 11:41:13 +0900

    Tab changing OK

--- Vietnamguide/map.js
+++ Vietnamguide/map.js
@@ -1,66 +1,32 @@
-/********************************************************************************
-
-	SYNCER 〜 知識、感動をみんなと同期(Sync)するブログ
-
-	* 配布場所
-	https://syncer.jp/google-maps-javascript-api-matome
-
-	* 最終更新日時
-	2015/08/03 19:24
-
-	* 作者
-	あらゆ
-
-	** 連絡先
-	Twitter: https://twitter.com/arayutw
-	Facebook: https://www.facebook.com/arayutw
-	Google+: https://plus.google.com/114918692417332410369/
-	E-mail: info@syncer.jp
-
-	※ バグ、不具合の報告、提案、ご要望など、お待ちしております。
-	※ 申し訳ありませんが、ご利用者様、個々の環境における問題はサポートしていません。
-
-********************************************************************************/
-
-//Topページ
 
 // キャンパスの要素を取得する
-var canvas = document.getElementById( 'map-canvas' ) ;
-var canvas_west_north = document.getElementById( 'map-canvas-west-north' ) ;
+var map;
+var map_west_north;
 
-// 中心の位置座標を指定する
-var latlng = new google.maps.LatLng( 16 , 107 );
-var latlng_west_north = new google.maps.LatLng( 17 , 108 );
+function initialize() {
+	// 中心の位置座標を指定する
+	var latlng = new google.maps.LatLng( 16 , 107 );
+	var latlng_west_north = new google.maps.LatLng( 23.5 , 102 );
 
-// 地図のオプションを設定する
-var mapOptions = {
-	zoom: 5 ,				// ズーム値
-	center: latlng ,		// 中心座標 [latlng]
-};
-var mapOptions_west_north = {
-	zoom: 5 ,				// ズーム値
-	center: latlng_west_north ,		// 中心座標 [latlng]
-};
+	// 地図のオプションを設定する
+	var mapOptions = {
+		zoom: 5 ,				// ズーム値
+		center: latlng ,		// 中心座標 [latlng]
+	};
+	var mapOptions_west_north = {
+		zoom: 7 ,				// ズーム値
+		center: latlng_west_north ,		// 中心座標 [latlng]
+	};
 
-// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-var map = new google.maps.Map( canvas , mapOptions ) ;
+	map = new google.maps.Map( document.getElementById( 'map-canvas' ) , mapOptions ) ;
+	map_west_north = new google.maps.Map( document.getElementById( 'map-canvas-west-north' ) , mapOptions_west_north ) ;
+}
 
-//西北部
-// キャンパスの要素を取得する
+//タブ切り替える時にリサイズする必要がある
+$("#to-west-north").on('shown.bs.tab', function() {
 
-
-// 中心の位置座標を指定する
-
-
-// 地図のオプションを設定する
-
-
-// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-var map_west_north = new google.maps.Map( canvas_west_north , mapOptions_west_north ) ;
-
-
-
-$(".tab").click(function(){
-    google.maps.event.trigger(map,'resize');
-    map.setCenter(latlng);
+  	/* Trigger map resize event */
+	google.maps.event.trigger(map_west_north, 'resize');
 });
+
+initialize();
